921122c4c45f1d3aadf13578c470a66759d84d6a
Tianyi Wang <t@tianyi.dev>
2020-10-15 10:25:34 +0100

    Add popular TV shows

--- src/App.js
+++ src/App.js
@@ -43,7 +43,9 @@ const IMAGE_URL = "https://image.tmdb.org/t/p/w780/";
 function App() {
   const classes = useStyles();
   const [popularMovies, setPopularMovies] = useState([]);
+  const [popularTVShows, setPopularTVShows] = useState([]);
   useEffect(() => {
+    // fetch movies
     const popular_movie_url =
       "https://api.themoviedb.org/3/movie/popular?api_key=b9e04ffd5a10a79d0459e43247be7805&language=en-US&include_adult=false";
 
@@ -51,6 +53,16 @@ function App() {
       .then((response) => response.json())
       .then((result) => {
         setPopularMovies(result.results);
+      });
+
+    // fetch TV shows
+    const popular_tv_url =
+      "https://api.themoviedb.org/3/tv/popular?api_key=b9e04ffd5a10a79d0459e43247be7805&language=en-US&include_adult=false";
+
+    fetch(popular_tv_url)
+      .then((response) => response.json())
+      .then((result) => {
+        setPopularTVShows(result.results);
       });
   }, []);
 
@@ -82,13 +94,39 @@ function App() {
                 <Card className={classes.card}>
                   <CardMedia
                     className={classes.cardMedia}
-                    style={{ height: 500 }}
-                    image={`${IMAGE_URL}${movie.poster_path}`}
+                    image={`${IMAGE_URL}${movie.backdrop_path}`}
                     title={movie.title}
                   />
                   <CardContent>
                     <Typography gutterBottom variant="h5" component="h2">
                       {movie.title}
+                    </Typography>
+                  </CardContent>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
+      </Box>
+
+      <Box mt={2} pt={2}>
+        <Container maxWidth="lg" component={Paper}>
+          <Typography variant="h5" className={classes.sectionTitle}>
+            Popular TV Shows
+          </Typography>
+
+          <Grid container spacing={2}>
+            {popularTVShows.map((show) => (
+              <Grid item xs={12} md={4} lg={3} key={show.id}>
+                <Card className={classes.card}>
+                  <CardMedia
+                    className={classes.cardMedia}
+                    image={`${IMAGE_URL}${show.backdrop_path}`}
+                    title={show.name}
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      {show.name}
                     </Typography>
                   </CardContent>
                 </Card>
