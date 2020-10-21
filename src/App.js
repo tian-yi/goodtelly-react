7b994b3e3aa34fbe125053ecf338c64de15766ea
Tianyi Wang <t@tianyi.dev>
2020-10-21 10:26:18 +0100

    Add SearchProgram component to power the TV Shows page and some minro style updates

--- src/App.js
+++ src/App.js
@@ -5,15 +5,14 @@ import { Switch, Route } from "react-router-dom";
 import { CssBaseline } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
-import Box from "@material-ui/core/Box";
 
-import { POPULAR_TV_URL } from "./config";
-import PopularList from "./PopularList";
+import { POPULAR_TV_URL, SEARCH_TV_URL } from "./config";
 import MovieDetails from "./MovieDetails";
 import TVDetails from "./TVDetails";
 import Home from "./Home";
 import Movie from "./Movie";
 import TopNav from "./TopNav";
+import SearchProgram from "./SearchProgram";
 
 const useStyles = makeStyles((theme) => ({
   main: {
@@ -27,7 +26,6 @@ function App() {
 
   useEffect(() => {
     // fetch TV shows
-
     fetch(POPULAR_TV_URL)
       .then((response) => response.json())
       .then((result) => {
@@ -55,12 +53,11 @@ function App() {
             <TVDetails />
           </Route>
           <Route path="/tv">
-            <Box mb={2}>
-              <PopularList
-                items={popularTVShows}
-                listTitle="Popular TV Shows"
-              />
-            </Box>
+            <SearchProgram
+              popularPrograms={popularTVShows}
+              searchURL={SEARCH_TV_URL}
+              title="TV Shows"
+            />
           </Route>
         </Switch>
       </Container>
---
+++ src/SearchProgram.js
@@ -0,0 +1,88 @@
+import React, { useEffect, useState } from "react";
+
+import PropTypes from "prop-types";
+import { makeStyles } from "@material-ui/core/styles";
+import Box from "@material-ui/core/Box";
+import TextField from "@material-ui/core/TextField";
+import FormControl from "@material-ui/core/FormControl";
+
+import { TMDB_IMAGE_URL } from "./config";
+import PopularList from "./PopularList";
+import Hero from "./Hero";
+
+const useStyles = makeStyles((theme) => ({
+  form: {
+    width: "100%",
+  },
+  formControl: {
+    opacity: 0.9,
+    width: "35%",
+    background: theme.palette.common.white,
+    borderRadius: theme.spacing(1),
+    padding: theme.spacing(2),
+  },
+}));
+
+const SearchProgram = ({ popularPrograms, searchURL, title }) => {
+  const [programs, setPrograms] = useState(popularPrograms);
+  const [heroImageURL, setHeroImageURL] = useState("");
+  const [searchText, setSearchText] = useState("");
+  const [sectionTitle, setSectionTitle] = useState(`Popular ${title}`);
+
+  useEffect(() => {
+    const randomProgramIndex = parseInt(Math.random() * 10, 10);
+    if (popularPrograms.length > 0) {
+      const randomTopProgramImage =
+        popularPrograms[randomProgramIndex].backdrop_path;
+      setHeroImageURL(`${TMDB_IMAGE_URL}w1280/${randomTopProgramImage}`);
+    }
+    setPrograms(popularPrograms);
+  }, [popularPrograms]);
+
+  const classes = useStyles();
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    fetch(`${searchURL}&query=${searchText}`)
+      .then((response) => response.json())
+      .then((result) => {
+        setPrograms(result.results);
+      });
+    setSectionTitle(`Search results for ${searchText}`);
+  };
+  return (
+    <>
+      <Hero heroImageURL={heroImageURL}>
+        <form
+          noValidate
+          autoComplete="off"
+          onSubmit={handleSearch}
+          className={classes.form}
+        >
+          <FormControl className={classes.formControl}>
+            <TextField
+              id="search"
+              label="Search"
+              type="search"
+              variant="outlined"
+              placeholder={`${title} name`}
+              value={searchText}
+              onChange={(e) => setSearchText(e.target.value)}
+            />
+          </FormControl>
+        </form>
+      </Hero>
+      <Box my={2}>
+        <PopularList items={programs} listTitle={sectionTitle} />
+      </Box>
+    </>
+  );
+};
+
+SearchProgram.propTypes = {
+  popularPrograms: PropTypes.array.isRequired,
+  searchURL: PropTypes.string.isRequired,
+  title: PropTypes.string,
+};
+
+export default SearchProgram;
